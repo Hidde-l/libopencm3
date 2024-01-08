c560e992fc31c36cd43f17407b13a92ba3cd81ac
Hidde <fuckoffpls@begone>
2024-01-08 16:07:35 +0100

    Got the basic setup done, now I can finally try to work on the assignments.
    Note: Currently I cannot work with registers because Im using opencm3, we might get a template for that later

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
+/**
+ * Student name: Hidde van Luenen
+ * Student number: 5734223
+ * 
+ */
+
 #include "utils.hpp"
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <stdbool.h>
-
+// #include "stm32f401xc.h"
+// #include "stm32f4xx.h"
 
 int main(void) {
     // Setup
@@ -16,8 +23,35 @@ int main(void) {
     while (true) {
       gpio_toggle(GPIOC, GPIO13);
       gpio_toggle(GPIOB, GPIO0);
-      ms_delay(100U);
+      ms_delay(500U);
     }
 
     return 0;
 }
+
+// #define LEDPIN 13
+// #define MASK(x) (1L << (x))
+// void setup(void);
+// void loop(void);
+
+
+// void setup (void) {
+//   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+//   GPIOC->MODER |= 0b01 << 26;
+//   GPIOC->OTYPER |= GPIO_OTYPER_OT13;
+// }
+
+// void loop (void) {
+//   // put your main code here , to run repeatedly :
+//   GPIOC->ODR ^= MASK(LEDPIN);
+//   ms_delay(300);
+// }
+
+
+// int main(void) {
+//   setup();
+//   while (1) {
+//     loop();
+//   }
+//   return 0;
+// }
